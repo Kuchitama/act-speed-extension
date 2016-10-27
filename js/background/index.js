9935a55b466a1a78519b023b4eb4ef3a2db781c6
Kuchitama <kiyo.kunihira@gmail.com>
2016-10-27 09:53:09 +0900

    Go to next lecture automatically

--- js/background/index.js
+++ js/background/index.js
@@ -1,18 +1,5 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.method == "loadSettings") {
-    var json = {
-      auto_play: true,
-      play_speed: 1.5
-    };
-    sendResponse(json);
-  } else {
-    sendResponse({});
-  }
-});
-
 function getData() {
   var data = localStorage['settings'];
-
   if (data) {
     return JSON.parse(data);
   } else {
@@ -20,7 +7,7 @@ function getData() {
   }
 }
 
-function setData(autoPlay, playSpeed) {
+function setData(autoPlay, playSpeed, autoNext) {
   var data = getData();
   if (autoPlay !== null) {
     data.auto_play = autoPlay;
@@ -28,9 +15,21 @@ function setData(autoPlay, playSpeed) {
   if (playSpeed !== null) {
     data.play_speed = playSpeed;
   }
+  if (autoNext !== null) {
+    data.auto_next = autoNext;
+  }
 
   localStorage['settings'] = JSON.stringify(data);
 }
 
-function setPlaySpeed(speed) { setData(null, speed); }
-function setAutoPlay(autoPlay) { setData(autoPlay, null); }
+function setPlaySpeed(speed) { setData(null, speed, null); }
+function setAutoPlay(autoPlay) { setData(autoPlay, null, null); }
+function setAutoNext(autoNext) { setData(null, null, autoNext); }
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.method == "loadSettings") {
+    sendResponse(getData());
+  } else {
+    sendResponse({});
+  }
+});
